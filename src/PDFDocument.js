b914d08d21d524d9d06d133d618230428bd6edfa
Omar Marín Pastor <omarmpastor@gmail.com>
2023-05-01 15:16:05 +0200

    Reformat

--- src/PDFDocument.js
+++ src/PDFDocument.js
@@ -7,27 +7,27 @@
  * @property {string} str
  */
 class PDFItem {
-    x;
-    y;
-    width;
-    height;
-    str;
+  x;
+  y;
+  width;
+  height;
+  str;
 
-    /**
-     * 
-     * @param {number} x 
-     * @param {number} y 
-     * @param {number} width 
-     * @param {number} height 
-     * @param {string} str 
-     */
-    constructor(x, y, width, height, str) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.str = str;
-    }
+  /**
+   *
+   * @param {number} x
+   * @param {number} y
+   * @param {number} width
+   * @param {number} height
+   * @param {string} str
+   */
+  constructor(x, y, width, height, str) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.str = str;
+  }
 }
 
 /**
@@ -35,51 +35,57 @@ class PDFItem {
  * @property {Array<PDFItem>} items
  */
 class PDFLine {
-    items;
-    numPagePDF;
-    /**
-     * 
-     * @param {number} numPagePDF 
-     */
-    constructor(numPagePDF) {
-        this.items = [];
-        this.numPagePDF = numPagePDF;
+  items;
+  numPagePDF;
+  /**
+   *
+   * @param {number} numPagePDF
+   */
+  constructor(numPagePDF) {
+    this.items = [];
+    this.numPagePDF = numPagePDF;
+  }
+
+  /**
+   *
+   * @param {PDFItem} item
+   */
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  /**
+   * @returns {number} the x of the first item
+   */
+  get x() {
+    if (this.items.length < 1) {
+      return -1;
     }
 
-    /**
-     * 
-     * @param {PDFItem} item 
-     */
-    addItem(item) {
-        this.items.push(item);
+    return this.items[0].x;
+  }
+
+  /**
+   * @returns {number} the y of the first item
+   */
+  get y() {
+    if (this.items.length < 1) {
+      return -1;
     }
 
-    /**
-     * @returns {number} the x of the first item
-     */
-    get x() {
-        if(this.items.length < 1) { return -1; }
+    return this.items[0].y;
+  }
 
-        return this.items[0].x;
-    }
-
-    /**
-     * @returns {number} the y of the first item
-     */
-    get y() {
-        if(this.items.length < 1) { return -1; }
-
-        return this.items[0].y;
-    }
-
-    /**
-     * @returns {string} concatenates the str property of all items separated by a space
-     */
-    get text() {
-        let str = "";
-        this.items.forEach(i => {str += i.str + (" ")});
-        return str.trim();
-    }
+  /**
+   * @returns {string} concatenates the str property of all items separated by a space
+   */
+  get text() {
+    let str = '';
+    this.items.forEach(i => {
+      str += i.str + ' ';
+    });
+    return str.trim();
+  }
 }
 
 /**
@@ -88,25 +94,25 @@ class PDFLine {
  * @property {Array<PDFLine>} lines
  */
 class PDFPage {
-    num;
-    lines;
+  num;
+  lines;
 
-    /**
-     * 
-     * @param {number} numPage 
-     */
-    constructor(numPage) {
-        this.num = numPage;
-        this.lines = [];
-    }
+  /**
+   *
+   * @param {number} numPage
+   */
+  constructor(numPage) {
+    this.num = numPage;
+    this.lines = [];
+  }
 
-    /**
-     * 
-     * @param {PDFLine} line 
-     */
-    addLine(line) {
-        this.lines.push(line);
-    }
+  /**
+   *
+   * @param {PDFLine} line
+   */
+  addLine(line) {
+    this.lines.push(line);
+  }
 }
 
 /**
@@ -115,46 +121,49 @@ class PDFPage {
  * @property {Array<PDFPage>} pages
  */
 class PDFDocument {
-    numPages;
-    pages;
-    
-    constructor() {
-        this.pages = [];
-    }
+  numPages;
+  pages;
 
-    /**
-     * 
-     * @param {PDFPage} page 
-     */
-    addPage(page) {
-        this.pages.push(page);
-    }
+  constructor() {
+    this.pages = [];
+  }
 
-    /**
-     * @returns {string} all text
-     */
-    get text() {
-        return this.pages.flatMap(p => p.lines).map(l => {
-            let str = "";
-            l.items.forEach(i => {str += i.str + (" ")});
-            return str.trim();
+  /**
+   *
+   * @param {PDFPage} page
+   */
+  addPage(page) {
+    this.pages.push(page);
+  }
+
+  /**
+   * @returns {string} all text
+   */
+  get text() {
+    return this.pages
+      .flatMap(p => p.lines)
+      .map(l => {
+        let str = '';
+        l.items.forEach(i => {
+          str += i.str + ' ';
         });
-    }
+        return str.trim();
+      });
+  }
 
-    /**
-     * Hay que revisarlo
-     */
-    deleteNumPages() {
-        this.pages.forEach(p => {
-            const lastLine = p.lines.length - 1;
-            const lastItem = p.lines[lastLine].items.length - 1;
+  /**
+   * Hay que revisarlo
+   */
+  deleteNumPages() {
+    this.pages.forEach(p => {
+      const lastLine = p.lines.length - 1;
+      const lastItem = p.lines[lastLine].items.length - 1;
 
-            if(p.lines[lastLine].items.length === 1 
-                && p.lines[lastLine].items[lastItem].str === p.num) {
-                p.lines.pop();
-            }
-        });
-    }
+      if (p.lines[lastLine].items.length === 1 && p.lines[lastLine].items[lastItem].str === p.num) {
+        p.lines.pop();
+      }
+    });
+  }
 }
 
 export { PDFItem, PDFLine, PDFPage, PDFDocument };
--- src/pdf-parse.js
+++ src/pdf-parse.js
@@ -5,111 +5,107 @@ import { PDFDocument, PDFPage, PDFLine, PDFItem } from './PDFDocument';
  * @param {ArrayBuffer} pdfPath
  * @returns {object} JSON
  */
-async function getPDFData (pdfBuffer) {
-    const pdfjsLib = require('pdfjs-dist/legacy/build/pdf')
-    const doc = await pdfjsLib.getDocument(pdfBuffer).promise;
+async function getPDFData(pdfBuffer) {
+  const pdfjsLib = require('pdfjs-dist/legacy/build/pdf');
+  const doc = await pdfjsLib.getDocument(pdfBuffer).promise;
 
-    const pdf = {
-        numPages: doc.numPages,
-        pages: []
+  const pdf = {
+    numPages: doc.numPages,
+    pages: [],
+  };
+
+  for (let i = 1; i <= doc.numPages; i++) {
+    const pagePDF = await doc.getPage(i);
+
+    const viewport = pagePDF.getViewport({ scale: 1.0 });
+    const pageInfo = {
+      num: i,
+      scale: viewport.scale,
+      rotation: viewport.rotation,
+      offsetX: viewport.offsetX,
+      offsetY: viewport.offsetY,
+      width: viewport.width,
+      height: viewport.height,
     };
 
-    for (let i = 1; i <= doc.numPages; i++) {
-        const pagePDF = await doc.getPage(i);
-
-        const viewport = pagePDF.getViewport({scale: 1.0});
-        const pageInfo = {
-            num: i,
-            scale: viewport.scale,
-            rotation: viewport.rotation,
-            offsetX: viewport.offsetX,
-            offsetY: viewport.offsetY,
-            width: viewport.width,
-            height: viewport.height
-        };
-        
-        const options = {
-            normalizeWhitespace: true,
-            disableCombineTextItems: false,
-        };
-        const content = await pagePDF.getTextContent(options); 
-        const page = {
-            num: pageInfo.num,
-            items: []
-        };
-        const allPageItems = content.items.map(item => {
-            const trs = item.transform;
-            let x = trs[4];
-            let y = pageInfo.height - trs[5];
-            if (viewport.rotation === 90) {
-                x = trs[5];
-                y = trs[4];
-            }
-            return {
-                x,
-                y,
-                width: item.width,
-                height: item.height,
-                str: item.str.trim()
-            };
-        });
-        
-        page.items = allPageItems.filter(itm => itm.width > 0 );
-
-        pdf.pages.push(page)
-    }
-
-    return pdf
-};
-
-/**
- * 
- * @param {object} page 
- * @returns {object}
- */
-function GroupAndOrderItemsOfPage(page) {
-    const itemsGroup = page.items.group(i => i.y);
-    const lines = [];
-    
-    Object.getOwnPropertyNames(itemsGroup).forEach(line => {
-        const lineOrderByX = itemsGroup[line].sort((a, b) => a.x - b.x);
-        lines.push(lineOrderByX);
+    const options = {
+      normalizeWhitespace: true,
+      disableCombineTextItems: false,
+    };
+    const content = await pagePDF.getTextContent(options);
+    const page = {
+      num: pageInfo.num,
+      items: [],
+    };
+    const allPageItems = content.items.map(item => {
+      const trs = item.transform;
+      let x = trs[4];
+      let y = pageInfo.height - trs[5];
+      if (viewport.rotation === 90) {
+        x = trs[5];
+        y = trs[4];
+      }
+      return {
+        x,
+        y,
+        width: item.width,
+        height: item.height,
+        str: item.str.trim(),
+      };
     });
 
-    return lines.sort((a, b) => a[0].y - b[0].y);
+    page.items = allPageItems.filter(itm => itm.width > 0);
+
+    pdf.pages.push(page);
+  }
+
+  return pdf;
 }
 
 /**
- * 
- * @param {string} pdfPath 
+ *
+ * @param {object} page
+ * @returns {object}
+ */
+function GroupAndOrderItemsOfPage(page) {
+  const itemsGroup = page.items.group(i => i.y);
+  const lines = [];
+
+  Object.getOwnPropertyNames(itemsGroup).forEach(line => {
+    const lineOrderByX = itemsGroup[line].sort((a, b) => a.x - b.x);
+    lines.push(lineOrderByX);
+  });
+
+  return lines.sort((a, b) => a[0].y - b[0].y);
+}
+
+/**
+ *
+ * @param {string} pdfPath
  * @returns {PDFDocument}
  */
 async function parsePDF(pdfPath) {
-    const pdfData = await getPDFData(pdfPath);
+  const pdfData = await getPDFData(pdfPath);
 
-    const pdfDoc = new PDFDocument();
-    pdfDoc.numPages = pdfData.numPages;
+  const pdfDoc = new PDFDocument();
+  pdfDoc.numPages = pdfData.numPages;
 
-    for(const page of pdfData.pages) {
-        const pdfPage = new PDFPage(page.num);
-        const linesData = GroupAndOrderItemsOfPage(page);
-        for(const lineData of linesData) {
-            const line = new PDFLine(page.num);
-            for(const itemData of lineData) {
-                line.addItem(
-                    new PDFItem(
-                        itemData.x, itemData.y,
-                        itemData.width, itemData.height,
-                        itemData.str
-                    )
-                );
-            }
-            pdfPage.addLine(line);
-        }
-        pdfDoc.addPage(pdfPage);
+  for (const page of pdfData.pages) {
+    const pdfPage = new PDFPage(page.num);
+    const linesData = GroupAndOrderItemsOfPage(page);
+    for (const lineData of linesData) {
+      const line = new PDFLine(page.num);
+      for (const itemData of lineData) {
+        line.addItem(
+          new PDFItem(itemData.x, itemData.y, itemData.width, itemData.height, itemData.str)
+        );
+      }
+      pdfPage.addLine(line);
     }
+    pdfDoc.addPage(pdfPage);
+  }
 
-    return pdfDoc;
+  return pdfDoc;
 }
 
 export default parsePDF;
